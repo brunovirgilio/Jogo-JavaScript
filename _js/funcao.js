a39e746c78f45d33d988bd73c26fcce5ac4474ff
Bruno Virgilio de Lima <bvdelima@gmail.com>
2022-04-29 06:29:55 -0300

    commit

--- _js/funcao.js
+++ _js/funcao.js
@@ -12,8 +12,8 @@ cont = 0
         function pergunta1(){
             nome=document.getElementById('resp').value
             document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-microscopio.jpg'
-            p.innerHTML='<h2>1. Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?</h2><p>1) Tem entre 2 a 4 litros. São retirados 450 mililitros<br>2) Tem entre 4 a 6 litros. São retirados 450 mililitros<br>3) Tem 10 litros. São retirados 2 litros<br>4)Tem 7 litros. São retirados 1,5 litros<br>5) Tem 0,5 litros. São retirados 0,5 litros<br></p>'
+            document.getElementById('capa').src='_imagens/1-TRANSITO.jpg'
+            p.innerHTML='<h2>1. De acordo com o Código de Trânsito Brasileiro (CTB) qual é a ordem de responsabilidade entre veículos?</h2><p>1) Bicicleta tem responsabilidade com o carro, que é responsável pela segurança do ônibus<br>2) Ônibus é responsável pelo carro, que é responsável pela segurança da bicicleta<br>3) Carro tem responsabilidade com a bicicleta, que é responsável pelo ônibus<br></p>'
             r1.innerHTML=' '
             r2.innerHTML=' '
             r3.innerHTML=' '
@@ -39,22 +39,22 @@ cont = 0
         
         function pergunta2(){
             document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-grecia2.jpg'
-            p.innerHTML='<h2>2. De quem é a famosa frase “Penso, logo existo”?</h2><p>1) Platão<br>2) Galileu Galilei<br>3) Descartes<br>4) Sócrates<br>5) Francis Bacon</p>'
+            document.getElementById('capa').src='_imagens/2-MOTORISTA.jpg'
+            p.innerHTML='<h2>2. O motorista comete qual infração quando estaciona o carro em ciclovia ou ciclofaixa?</h2><p>1) Média, com penalidade de 4 pontos na carteira de motorista<br>2) Grave, sujeita a multa de guincho para retirada do veículo<br>3) Apenas uma medida administrativa, sujeita a remoção do veículo do local<br></p>'
             r1.innerHTML=' '
             r2.innerHTML=' '
             r3.innerHTML=' '
             r2.innerHTML='<p onclick="confirma2()">Confirma</p>'            
         }
         function confirma2(){
-            if(Number(n.value)==3){
+            if(Number(n.value)==2){
                 cont++
                 r1.innerHTML='Resposta Correta'
                 r1.style.background='green'
                 r1.style.color='white'
             }
             else{
-                r1.innerHTML='Resposta Errada, a opção 3 é a correta.'
+                r1.innerHTML='Resposta Errada, a opção 2 é a correta.'
                 r1.style.background='red'
                 r1.style.color='white'
             }
@@ -63,8 +63,8 @@ cont = 0
         }
         function pergunta3(){
             document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-chuveiro3.jpg'
-            p.innerHTML='<h2>3. De onde é a invenção do chuveiro elétrico?</h2><p>1) França<br>2) Inglaterra<br>3) Brasil<br>4) Austrália<br>5) Itália</p>'
+            document.getElementById('capa').src='_imagens/3-BICICLETA.jpg'
+            p.innerHTML='<h2>3. Em qual situação é permitido andar de bicicleta na calçada?</h2><p>1) Apenas quando a via estiver engarrafada<br>2) Quando acontece algum acidente que obstrui a passagem do ciclista<br>3) Quando a bicicleta estiver devidamente sinalizada e autorizada pelo órgão que tem responsabilidade pela calçada</p>'
             r1.innerHTML=' '
             r2.innerHTML=' '
             r3.innerHTML=' '
@@ -87,14 +87,62 @@ cont = 0
         }
         function pergunta4(){
             document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-mapa4.jpg'
-            p.innerHTML='<h2>4. Quais o menor e o maior país do mundo?</h2><p>1) Vaticano e Rússia<br>2) Nauru e China<br>3) Mônaco e Canadá<br>4) Malta e Estados Unidos<br>5) São Marino e Índia</p>'
+            document.getElementById('capa').src='_imagens/4-PEDESTRE.jpg'
+            p.innerHTML='<h2>4. Qual destas condutas não é indicada para um pedestre?</h2><p>1) Atravessar a rua em linha reta<br>2) Atravessar fora da faixa ou da passarela<br>3) Evitar ficar próximo ao meio fio no deslocamento pela calçada</p>'
             r1.innerHTML=' '
             r2.innerHTML=' '
             r3.innerHTML=' '
             r2.innerHTML='<p onclick="confirma4()">Confirma</p>'      
         }
         function confirma4(){
+            if(Number(n.value)==2){
+                cont++
+                r1.innerHTML='Resposta Correta'
+                r1.style.background='green'
+                r1.style.color='white'
+            }
+            else{
+                r1.innerHTML='Resposta Errada, a opção 2 é a correta.'
+                r1.style.background='red'
+                r1.style.color='white'
+            }
+            r2.innerHTML=''    
+            r3.innerHTML='<p onclick="pergunta5()">Próximo</p>'
+        }
+        function pergunta5(){
+            document.getElementById('resp').value=' '
+            document.getElementById('capa').src='_imagens/5-MOTO.jpg'
+            p.innerHTML='<h2>5. É permitido transportar caixas, sacolas e outros volumes na motocicleta, sem utilização de baú fixado?</h2><p>1) Não, só é permitido carregar objetos dentro do baú ou bagageiro, para segurança do piloto e dos demais veículos<br>2) É permitido, mas a dimensão dos volumes não pode ser maior do que as costas do motociclista<br>3) É permitido apenas em vias cuja velocidade máxima permitida seja 40km/h</p>'
+            r1.innerHTML=' '
+            r2.innerHTML=' '
+            r3.innerHTML=' '
+            r2.innerHTML='<p onclick="confirma5()">Confirma</p>'      
+        }
+        function confirma5(){
+            if(Number(n.value)==2){
+                cont++
+                r1.innerHTML='Resposta Correta'
+                r1.style.background='green'
+                r1.style.color='white'
+            }
+            else{
+                r1.innerHTML='Resposta Errada, a opção 2 é a correta.'
+                r1.style.background='red'
+                r1.style.color='white'
+            }
+            r2.innerHTML=''    
+            r3.innerHTML='<p onclick="pergunta6()">Próximo</p>'
+        }
+        function pergunta6(){
+            document.getElementById('resp').value=' '
+            document.getElementById('capa').src='_imagens/6-MAO-MOTO.jpg'
+            p.innerHTML='<h2>6. Em qual situação segurar o guidão da motocicleta apenas com uma mão é permitida?</h2><p>1) Apenas quando, eventualmente, necessitar sinalizar uma manobra<br>2) Nenhuma<br>3) Quando o motociclista estiver com formigamento ou câimbra em uma da mãos<br></p>'
+            r1.innerHTML=' '
+            r2.innerHTML=' '
+            r3.innerHTML=' '
+            r2.innerHTML='<p onclick="confirma6()">Confirma</p>'      
+        }
+        function confirma6(){
             if(Number(n.value)==1){
                 cont++
                 r1.innerHTML='Resposta Correta'
@@ -107,90 +155,18 @@ cont = 0
                 r1.style.color='white'
             }
             r2.innerHTML=''    
-            r3.innerHTML='<p onclick="pergunta5()">Próximo</p>'
-        }
-        function pergunta5(){
-            document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-brasilia5.jpg'
-            p.innerHTML='<h2>5. Qual o nome do presidente do Brasil que ficou conhecido como Jango?</h2><p>1) Jânio Quadros<br>2) Jacinto Anjos<br>3) Getúlio Vargas<br>4) João Figueiredo<br>5) João Goulart</p>'
-            r1.innerHTML=' '
-            r2.innerHTML=' '
-            r3.innerHTML=' '
-            r2.innerHTML='<p onclick="confirma5()">Confirma</p>'      
-        }
-        function confirma5(){
-            if(Number(n.value)==5){
-                cont++
-                r1.innerHTML='Resposta Correta'
-                r1.style.background='green'
-                r1.style.color='white'
-            }
-            else{
-                r1.innerHTML='Resposta Errada, a opção 5 é a correta.'
-                r1.style.background='red'
-                r1.style.color='white'
-            }
-            r2.innerHTML=''    
-            r3.innerHTML='<p onclick="pergunta6()">Próximo</p>'
-        }
-        function pergunta6(){
-            document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-palavra6.jpg'
-            p.innerHTML='<h2>6. Qual o grupo em que todas as palavras foram escritas corretamente?</h2><p>1) Asterístico, beneficiente, meteorologia, entertido<br>2) Asterisco, beneficente, meteorologia, entretido<br>3) Asterisco, beneficente, metereologia, entretido<br>4) Asterístico, beneficiente, metereologia, entretido<br>5) Asterisco, beneficiente, metereologia, entretido</p>'
-            r1.innerHTML=' '
-            r2.innerHTML=' '
-            r3.innerHTML=' '
-            r2.innerHTML='<p onclick="confirma6()">Confirma</p>'      
-        }
-        function confirma6(){
-            if(Number(n.value)==2){
-                cont++
-                r1.innerHTML='Resposta Correta'
-                r1.style.background='green'
-                r1.style.color='white'
-            }
-            else{
-                r1.innerHTML='Resposta Errada, a opção 2 é a correta.'
-                r1.style.background='red'
-                r1.style.color='white'
-            }
-            r2.innerHTML=''    
             r3.innerHTML='<p onclick="pergunta7()">Próximo</p>'
         }
         function pergunta7(){
             document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-livro7.jpg'
-            p.innerHTML='<h2>7. Qual o livro mais vendido no mundo a seguir à Bíblia?</h2><p>1) O Senhor dos Anéis<br>2) Dom Quixote<br>3) O Pequeno Príncipe<br>4) Ela, a Feiticeira<br>5) Um Conto de Duas Cidades</p>'
+            document.getElementById('capa').src='_imagens/7-RURAL.jpg'
+            p.innerHTML='<h2>7. Como deve ser a circulação de pedestres em vias rurais sem acostamento?</h2><p>1)No meio da pista, quando veículos não estiverem circulando<br>2) No mesmo sentido dos veículos, na extremidade da pista<br>3) Nas bordas da pista, em sentido contrário ao de circulação de carros, exceto em locais que a sinalização não permita</p>'
             r1.innerHTML=' '
             r2.innerHTML=' '
             r3.innerHTML=' '
             r2.innerHTML='<p onclick="confirma7()">Confirma</p>'      
         }
         function confirma7(){
-            if(Number(n.value)==2){
-                cont++
-                r1.innerHTML='Resposta Correta'
-                r1.style.background='green'
-                r1.style.color='white'
-            }
-            else{
-                r1.innerHTML='Resposta Errada, a opção 2 é a correta.'
-                r1.style.background='red'
-                r1.style.color='white'
-            }
-            r2.innerHTML=''    
-            r3.innerHTML='<p onclick="pergunta8()">Próximo</p>'
-        }
-        function pergunta8(){
-            document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-aula8.jpg'
-            p.innerHTML='<h2>8. Quantas casas decimais tem o número pi?</h2><p>1) Duas<br>2) Centenas<br>3) Infinitas<br>4) Vinte<br>5) Milhares</p>'
-            r1.innerHTML=' '
-            r2.innerHTML=' '
-            r3.innerHTML=' '
-            r2.innerHTML='<p onclick="confirma8()">Confirma</p>'      
-        }
-        function confirma8(){
             if(Number(n.value)==3){
                 cont++
                 r1.innerHTML='Resposta Correta'
@@ -203,26 +179,50 @@ cont = 0
                 r1.style.color='white'
             }
             r2.innerHTML=''    
-            r3.innerHTML='<p onclick="pergunta9()">Próximo</p>'
+            r3.innerHTML='<p onclick="pergunta8()">Próximo</p>'
         }
-        function pergunta9(){
+        function pergunta8(){
             document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-quimica9.jpg'
-            p.innerHTML='<h2>9. Atualmente, quantos elementos químicos a tabela periódica possui?</h2><p>1) 113<br>2) 109<br>3) 108<br>4) 118<br>5) 92<br></p>'
+            document.getElementById('capa').src='_imagens/8-POLICIAL.jpg'
+            p.innerHTML='<h2>8. Cidadãos podem sugerir implantação de equipamentos e alteração de normas de sinalização e trânsito?</h2><p>1) Não<br>2) Sim, em todas as situações o cidadão pode sugerir fiscalização, sinalização e sugerir normas<br>3) Sim, mas apenas quando campanhas de trânsito estiverem vigentes</p>'
             r1.innerHTML=' '
             r2.innerHTML=' '
             r3.innerHTML=' '
-            r2.innerHTML='<p onclick="confirma9()">Confirma</p>'      
+            r2.innerHTML='<p onclick="confirma8()">Confirma</p>'      
         }
-        function confirma9(){
-            if(Number(n.value)==4){
+        function confirma8(){
+            if(Number(n.value)==2){
                 cont++
                 r1.innerHTML='Resposta Correta'
                 r1.style.background='green'
                 r1.style.color='white'
             }
             else{
-                r1.innerHTML='Resposta Errada, a opção 4 é a correta. '
+                r1.innerHTML='Resposta Errada, a opção 2 é a correta.'
+                r1.style.background='red'
+                r1.style.color='white'
+            }
+            r2.innerHTML=''    
+            r3.innerHTML='<p onclick="pergunta9()">Próximo</p>'
+        }
+        function pergunta9(){
+            document.getElementById('resp').value=' '
+            document.getElementById('capa').src='_imagens/9-ACIDENTE.jpg'
+            p.innerHTML='<h2>9. Caso uma pessoa esteja dirigindo um carro que não é seu e cometa alguma infração de trânsito, quem será multado?</h2><p>1) O condutor e o proprietário<br>2) O proprietário<br>3) O condutor</p>'
+            r1.innerHTML=' '
+            r2.innerHTML=' '
+            r3.innerHTML=' '
+            r2.innerHTML='<p onclick="confirma9()">Confirma</p>'      
+        }
+        function confirma9(){
+            if(Number(n.value)==3){
+                cont++
+                r1.innerHTML='Resposta Correta'
+                r1.style.background='green'
+                r1.style.color='white'
+            }
+            else{
+                r1.innerHTML='Resposta Errada, a opção 3 é a correta. '
                 r1.style.background='red'
                 r1.style.color='white'
             }
@@ -231,8 +231,8 @@ cont = 0
         }
         function pergunta10(){
             document.getElementById('resp').value=' '
-            document.getElementById('capa').src='_imagens/foto-idoso10.jpg'
-            p.innerHTML='<h2>10. Quais os países que têm a maior e a menor expectativa de vida do mundo?</h2><p>1) Japão e Serra Leoa<br>2) Austrália e Afeganistão<br>3) Itália e Chade<br>4) Brasil e Congo<br>5) Estados Unidos e Angola</p>'
+            document.getElementById('capa').src='_imagens/10-ENGARRAFAMENTO.jpg'
+            p.innerHTML='<h2>10. Qual região do país tem o maior tempo médio de deslocamento entre casa-trabalho?</h2><p>1) Grande São Paulo<br>2) Região Metropolitana do Rio de Janeiro<br>3) Grande BH</p>'
             r1.innerHTML=' '
             r2.innerHTML=' '
             r3.innerHTML=' '
